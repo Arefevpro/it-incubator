2d828d8989b7d4c106bd7ce337341e10201a5761
Arefevpro <arefevmsk@gmail.com>
2024-02-07 23:21:47 +0300

    ./

--- portfolio/src/components/icon/Icon.tsx
+++ portfolio/src/components/icon/Icon.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import iconsSprite from "../../assets/icons-sprite.svg";
+import iconsSprite from "../../assets/images/icons-sprite.svg";
 
 
 type IconPropsType = {
   iconId: string;
+  width?: string;
+  height?: string;
+  viewbox?: string;
 }
 
 export const Icon = (props: IconPropsType) => {
   return (
-    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+    <svg width={props.width || "50"}  height={props.height || "50"} viewBox={props.viewbox ||  "0 0 50 50"}  fill="none" xmlns="http://www.w3.org/2000/svg"> 
     <use xlinkHref={ `${ iconsSprite }#${props.iconId}` }/> 
     </svg>
   );
