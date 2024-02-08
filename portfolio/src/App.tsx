0e91c3e5900563b9c18f17b86e7e3550cb8a6679
Arefevpro <arefevmsk@gmail.com>
2024-02-08 14:35:42 +0300

    ./

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css';
 import styled from 'styled-components';
 import { Header } from './layout/header/Header';
-
+import { Main } from './layout/header/sections/main/Main';
 
 function App() {
     return (
         <div className="App">
+            <StylGlobal>
             <Header />
+            <Main />
+            </StylGlobal>
         </div>
     );
 }
 
 export default App;
 
+export const StylGlobal = styled.div`
+  background-color: #2b2e31;
+  color: white;
+`;
--- portfolio/src/layout/header/sections/main/Main.tsx
+++ portfolio/src/layout/header/sections/main/Main.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from 'styled-components';
+import photoP1 from '../../../../assets/images/Preview1.webp';
 
 
 export const Main = ( ) => {
@@ -12,7 +14,7 @@ export const Main = ( ) => {
         </h1>
       <p>He crafts responsive websites where technologies meet creativity</p>
       <button>Contact me!!</button>
-      <img src="" alt="" />
+      <Photo src={photoP1} alt="" />
       <h2>Currently working on 
         <span>Portfolio</span>
       </h2>
@@ -20,3 +22,9 @@ export const Main = ( ) => {
   </div>
   );
 };
+
+const Photo = styled.img`
+  width: 457px;
+  height: 386px;
+  object-fit: cover;
+`;
