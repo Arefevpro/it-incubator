75032a172257303ae1a977ad9a56b62d3cbf45e3
Arefevpro <arefevmsk@gmail.com>
2024-02-08 14:51:45 +0300

    ./

--- portfolio/src/layout/header/sections/main/Main.tsx
+++ portfolio/src/layout/header/sections/main/Main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styled from 'styled-components';
 import photoP1 from '../../../../assets/images/Preview1.webp';
+import { FlexWrapper } from "../../../../components/FlexWrapper";
 
 
 export const Main = ( ) => {
   return (
   <div>
-    <div>
-      <h1>Elias is a 
+    <FlexWrapper>
+   <div>
+   <h1>Elias is a 
         <span>web designer</span> 
          and 
         <span>front-end developer</span>
@@ -18,7 +20,8 @@ export const Main = ( ) => {
       <h2>Currently working on 
         <span>Portfolio</span>
       </h2>
-    </div>
+   </div>
+    </FlexWrapper>
   </div>
   );
 };
