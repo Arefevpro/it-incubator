a1532f2c8897295ece324f1e1c89acaaeaa29afb
Arefevpro <arefevmsk@gmail.com>
2024-02-08 14:21:46 +0300

    ./

--- portfolio/src/layout/header/sections/main/Main.tsx
+++ portfolio/src/layout/header/sections/main/Main.tsx
@@ -5,8 +5,17 @@ export const Main = ( ) => {
   return (
   <div>
     <div>
-      <span></span>
-      <span></span>
+      <h1>Elias is a 
+        <span>web designer</span> 
+         and 
+        <span>front-end developer</span>
+        </h1>
+      <p>He crafts responsive websites where technologies meet creativity</p>
+      <button>Contact me!!</button>
+      <img src="" alt="" />
+      <h2>Currently working on 
+        <span>Portfolio</span>
+      </h2>
     </div>
   </div>
   );
