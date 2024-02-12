298761579430c11695862eab0e1cbaba181384e5
Arefevpro <arefevmsk@gmail.com>
2024-02-12 17:32:12 +0300

    ./

--- portfoliotwo/src/layout/header/Header.tsx
+++ portfoliotwo/src/layout/header/Header.tsx
@@ -1,33 +1,15 @@
 import React from "react";
 import styled from "styled-components"
 import { Logo } from "../../components/logo/Logo"
+import { Menu } from "../../components/menu/Menu"
 
 
 export const Header = () => {
   return (
     <StyledHeader>
     <Logo />
-
-      <nav>
-        <ul>
-          <li>
-            <a href="">Home</a>
-            </li>
-          <li>
-            <a href="">Skills</a>
-            </li>
-          <li>
-            <a href="">Works</a>
-            </li>
-          <li>
-            <a href="">Testimony</a>
-            </li>
-          <li>
-            <a href="">Contact</a>
-            </li>
-        </ul>
-      </nav>
-    </StyledHeader>
+    <Menu />
+    </StyledHeader> 
   );
 }
 
