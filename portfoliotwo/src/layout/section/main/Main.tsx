537a5499b2298238cbceb6a21527a6b61b332597
Arefevpro <arefevmsk@gmail.com>
2024-02-13 10:16:52 +0300

    ./

--- portfoliotwo/src/layout/section/main/Main.tsx
+++ portfoliotwo/src/layout/section/main/Main.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import styled from "styled-components"
 import photo from "../../../assets/images/photo.webp"
+import { FlexWrapper } from "../../../components/Flexwrapper"
 
 
 export const Main = () => {
   return (
-    <div>
-      <div>
-        <span>Hi There</span>
-        <h2>I am Svetlana Dyablo</h2>
-        <h1>A Web Developer.</h1>
-      </div>
-      <img src="" alt="" />
-    </div>
+    <Styledmain>
+      <FlexWrapper alignItems='center' justifyContent='space-around'>
+        <div>
+          <span>Hi There</span>
+          <h2>I am Svetlana Dyablo</h2>
+          <h1>A Web Developer.</h1>
+        </div>
+          <Photo src={photo} alt="" />
+      </FlexWrapper>
+    </Styledmain>
   );
 };
+
+
+const Styledmain = styled.div`
+min-height: 100vh;
+background-color: #3d9ea3;
+`
+
 
 
 const Photo = styled.img`
   width: 350px;
   height: 430px;
+  object-fit: cover;
 `
