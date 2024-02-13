82272b611e54115a13763978ba49b21dabecd43f
Arefevpro <arefevmsk@gmail.com>
2024-02-13 09:51:25 +0300

    ./

--- portfoliotwo/src/layout/section/main/Main.tsx
+++ portfoliotwo/src/layout/section/main/Main.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 export const Main = () => {
   return (
     <div>
-      
+      <div>
+        <span>Hi There</span>
+        <h2>I am Svetlana Dyablo</h2>
+        <h1>A Web Developer.</h1>
+      </div>
     </div>
   );
 };
 
-export default Main;
