0d94ac0da5a041078bea7140b1538bde317528f6
Arefevpro <arefevmsk@gmail.com>
2024-02-13 10:21:43 +0300

    ./

--- portfoliotwo/src/layout/section/main/Main.tsx
+++ portfoliotwo/src/layout/section/main/Main.tsx
@@ -10,8 +10,8 @@ export const Main = () => {
       <FlexWrapper alignItems='center' justifyContent='space-around'>
         <div>
           <span>Hi There</span>
-          <h2>I am Svetlana Dyablo</h2>
-          <h1>A Web Developer.</h1>
+          <Name>I am Svetlana Dyablo</Name>
+          <MainTitle>A Web Developer.</MainTitle>
         </div>
           <Photo src={photo} alt="" />
       </FlexWrapper>
@@ -22,7 +22,7 @@ export const Main = () => {
 
 const Styledmain = styled.div`
 min-height: 100vh;
-background-color: #3d9ea3;
+background-color: #133911;
 `
 
 
@@ -31,4 +31,11 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+  color: #ffffff;
+`
+const Name = styled.h2`
+  color: #ffffff;
 `
