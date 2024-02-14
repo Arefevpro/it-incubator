3b78706365f10acd8ebf1ba67bd32b589fc2336c
Arefevpro <arefevmsk@gmail.com>
2024-02-14 22:43:02 +0300

    ./

--- portfoliotwo/src/components/slider/Slider.tsx
+++ portfoliotwo/src/components/slider/Slider.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import styled from "styled-components"
-
+import { FlexWrapper } from '../Flexwrapper';
 
 export const Slider = () => {
   return (
     <StyledSlider>
+      <FlexWrapper>
       <Slide>
         <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         </Text>
         <Name>@ivan ivanow</Name>
       </Slide>
+      </FlexWrapper>
+      <Pagination>
+        <span></span>
+        <span></span>
+        <span></span>
+      </Pagination>
     </StyledSlider>
   );
 };
@@ -35,6 +42,12 @@ const Text = styled.p`
 `
 
 const Name = styled.span`
+  box-sizing: border-box;
+  border: #ed0ce2 2px solid;
+  background-color: #a6c9cc;
+`
+
+const Pagination = styled.span`
   box-sizing: border-box;
   border: #ed0ce2 2px solid;
   background-color: #a6c9cc;
