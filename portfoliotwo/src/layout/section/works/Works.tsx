ebe5eb83ef136b19556edadc3ce231f5cbb9adb9
Arefevpro <arefevmsk@gmail.com>
2024-02-14 19:01:02 +0300

    ./

--- portfoliotwo/src/layout/section/works/Works.tsx
+++ portfoliotwo/src/layout/section/works/Works.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from "styled-components"
 import { Menu } from '../../../components/menu/Menu';
+import { SectionTitle } from '../../../components/SectionTitle';
+
+const worksItems = ["All", "landing page", "React", "spa"]
 
 export const Works = () => {
   return (
     <StyledWorks>
-      {/* <SectionTitle>My Works</SectionTitle> */}
+      <SectionTitle>My Works</SectionTitle>
       <Menu />
     </StyledWorks>
   );
