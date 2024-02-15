808ff344f4a950130c00dec2d833cd2e5122e7fe
Arefevpro <arefevmsk@gmail.com>
2024-02-15 16:22:16 +0300

    ./

--- portfoliotwo/src/layout/section/slogan/Slogan.tsx
+++ portfoliotwo/src/layout/section/slogan/Slogan.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { SectionTitle } from '../../../components/SectionTitle';
+import { Button } from '../../../components/Button';
 
 export const Slogan = () => {
   return (
     <StyletSlogan>
-      
+      <SectionTitle>I Am Available For Freelance</SectionTitle>
+      <Button>Hire me</Button>
     </StyletSlogan>
   );
 };
 
 const StyletSlogan = styled.section`
-  
+  min-height: 40vh;
+  background-color: #a6c9cc;
+  box-sizing: border-box;
+  border: #ed0ce2 2px solid;
 `
