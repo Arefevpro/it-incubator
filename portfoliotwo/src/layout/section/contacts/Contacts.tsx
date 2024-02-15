56aa6fc2210db6b7a75c63e9c9eb84cbe121b75c
Arefevpro <arefevmsk@gmail.com>
2024-02-15 10:00:17 +0300

    ./

---
+++ portfoliotwo/src/layout/section/contacts/Contacts.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Contacts = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+const StyledContacts = styled.section `
+  
+  
+`
