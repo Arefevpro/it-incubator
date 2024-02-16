0facae83b8a6095612d38f83027ba6a59689d850
Arefevpro <arefevmsk@gmail.com>
2024-02-16 10:49:04 +0300

    ./

--- portfoliotwo/src/styles/Global.styled.tsx
+++ portfoliotwo/src/styles/Global.styled.tsx
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyled = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   *,
   *::before,
   *::after {
