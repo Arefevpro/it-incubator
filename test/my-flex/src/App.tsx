067fac9bd2bcded16ba2846a608052a7deca9d6d
Arefevpro <arefevmsk@gmail.com>
2024-02-23 19:33:09 +0300

    ./

---
+++ test/my-flex/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="myDiv">I`m div</div>
+      <img className="myImg" src="./img/Adventure-PNG-Pic.png" alt="" />
+      <input className="myInput" type="text" placeholder="i`m input" />
+      <p className="myText">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        quo assumenda? Corporis, perspiciatis, magnam quod eum recusandae
+        asperiores, debitis laudantium eligendi velit ex hic exercitationem
+        libero cupiditate quo iusto iure.
+      </p>
+      <span className="mySpan">i`m span</span>
+    </div>
+  );
+}
+
+export default App;
