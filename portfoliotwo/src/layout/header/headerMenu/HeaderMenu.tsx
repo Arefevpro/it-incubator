b50b71c07e6b3daf99f38b0b6b2468231cc80262
Arefevpro <arefevmsk@gmail.com>
2024-02-24 19:55:46 +0300

    ./

--- portfoliotwo/src/layout/header/headerMenu/HeaderMenu.tsx
+++ portfoliotwo/src/layout/header/headerMenu/HeaderMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components"
+import { theme} from "../../../styles/Theme";
 
 
 export const HeaderMenu = (props:  {menuItems: Array<string>} ) => {
@@ -35,7 +36,7 @@ const StyledHeaderMenu = styled.nav`
   }
 `
 const ListItem = styled.li`
-
+  position: relative;
 `
 
 const Link = styled.a`
@@ -49,5 +50,12 @@ const Link = styled.a`
 `
 
 const Mask = styled.span`
-
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: inline-block;
+  height: 50%;
+  overflow-y: hidden;
+  outline: 1px solid rgba(117, 114, 213, 1);
+  color: ${theme.colors.accent};
   `
