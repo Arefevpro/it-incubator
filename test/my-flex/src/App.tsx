cedda1b3b066b260c018705506bbba24b8154002
Arefevpro <arefevmsk@gmail.com>
2024-02-24 14:10:28 +0300

    ./

--- test/my-flex/src/App.tsx
+++ test/my-flex/src/App.tsx
@@ -4,17 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="myDiv">I`m div</div>
-      <img className="myImg" src="./img/Adventure-PNG-Pic.png" alt="">img</img>
-      <input className="myInput" type="text" placeholder="i`m input" />
-      <p className="myText">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
-        quo assumenda? Corporis, perspiciatis, magnam quod eum recusandae
-        asperiores, debitis laudantium eligendi velit ex hic exercitationem
-        libero cupiditate quo iusto iure.
-      </p>
-      <span className="mySpan">i`m span</span>
-    </div>
+      <div className="flex-container">
+        <div className="myDiv">I`m div</div>
+        <img
+          className="myImg"
+          src="https://www.pngmart.com/files/7/Discount-PNG-Background-Image.png"
+          alt=""
+        />
+        <input className="myInput" type="text" placeholder="i`m input" />
+        <p className="myText">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+          quo assumenda? Corporis, perspiciatis, magnam quod eum recusandae
+          asperiores, debitis laudantium eligendi velit ex hic exercitationem
+          libero cupiditate quo iusto iure.
+        </p>
+        <span className="mySpan">i`m span</span>
+      </div>
+
+     {/* <div className="wrap">
+     <header>Header</header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </div> */}
+     </div>
   );
 }
 
