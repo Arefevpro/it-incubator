fdffbb6453078f617f51a223f2534e5cc112fed0
Arefevpro <arefevmsk@gmail.com>
2024-02-24 17:33:32 +0300

    ./

--- test/my-flex/src/App.tsx
+++ test/my-flex/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="flex-container">
+      {/* <div className="flex-container">
         <div className="myDiv">I`m div</div>
         <img
           className="myImg"
@@ -19,13 +19,32 @@ function App() {
           libero cupiditate quo iusto iure.
         </p>
         <span className="mySpan">i`m span</span>
-      </div>
+      </div> */}
 
      {/* <div className="wrap">
      <header>Header</header>
       <main>Main</main>
       <footer>Footer</footer>
     </div> */}
+    
+
+    <ul>
+      <li>Первый</li>
+      <li>Второй</li>
+      <li>Последний</li>
+    </ul>
+    
+    <div>
+      <h2>Первый заголовок</h2>
+      <p>Первый абзац</p>
+      <h2>Второй заголовок</h2>
+      <p>Второй абзац</p>
+      <p>Третий абзац</p>
+      <h2>Последний заголовок</h2>
+      <p>Последний абзац</p>
+    </div>
+
+    
      </div>
   );
 }
