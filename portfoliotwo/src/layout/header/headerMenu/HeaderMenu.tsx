254a1676727fb23ab71d84a49576a900f3ee49c0
Arefevpro <arefevmsk@gmail.com>
2024-02-24 19:48:13 +0300

    ./

--- portfoliotwo/src/layout/header/headerMenu/HeaderMenu.tsx
+++ portfoliotwo/src/layout/header/headerMenu/HeaderMenu.tsx
@@ -4,23 +4,50 @@ import styled from "styled-components"
 
 export const HeaderMenu = (props:  {menuItems: Array<string>} ) => {
   return (
-    <StyledMenu>
-        <ul>
-          {props.menuItems.map((item: string, index: number) => {
-            return <li key={index}>
-              <a href="#">{item}</a>
-            </li>
-          })}
-        </ul>
-      </StyledMenu>
+    <StyledHeaderMenu>
+      <ul>
+        {props.menuItems.map((item: string, index: number) => {
+          return (
+            <ListItem key={index}>
+              <Link href="#">
+                {item}
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+              </Link>
+            </ListItem>
+          );
+        })}
+      </ul>
+    </StyledHeaderMenu>
   );
 };
 
 
-const StyledMenu = styled.nav`
+const StyledHeaderMenu = styled.nav`
   ul {
     display: flex;
     gap: 30px;
     justify-content: center;
   }
 `
+const ListItem = styled.li`
+
+`
+
+const Link = styled.a`
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+  line-height: 55px;
+  text-align: center;
+  color: rgba(117, 114, 213, 1);
+;
+`
+
+const Mask = styled.span`
+
+  `
