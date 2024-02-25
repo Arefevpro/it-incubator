8441dd89995933fa5a11f6d31c7ca7bac3afa635
Arefevpro <arefevmsk@gmail.com>
2024-02-25 23:32:55 +0300

    ./

--- portfoliotwo/src/layout/section/main/Main.tsx
+++ portfoliotwo/src/layout/section/main/Main.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import photo from "../../../assets/images/photo.webp"
 import { FlexWrapper } from "../../../components/Flexwrapper"
 import { Container } from '../../../components/slider/Container';
+import { theme } from '../../../styles/Theme';
 
 
 export const Main = () => {
@@ -12,7 +13,7 @@ export const Main = () => {
         <FlexWrapper alignItems="center" justifyContent="space-between">
           <div>
             <SmallText>Hi There</SmallText>
-            <Name>I am Svetlana Dyablo</Name>
+            <Name>I am <span>Svetlana Dyablo</span></Name>
             <MainTitle>A Web Developer.</MainTitle>
           </div>
           <Photo src={photo} alt="" />
@@ -40,10 +41,22 @@ font-weight: 400;
 line-height: 41px;
 `
 const Name = styled.h2`
-  font-family: 'Josefin Sans', sans-serif;
+  font-family: "Josefin Sans", sans-serif;
   font-size: 50px;
   font-weight: 700;
   letter-spacing: 0.05em;
+  margin: 10px 0;
+
+  span {
+    &::before {
+      content: "";
+      display: inline-block;
+      /* position: absolute; */
+      background-color: ${theme.colors.accent};
+      height: 20px;
+      width: 100%;
+    }
+  }
 `
 
 const SmallText = styled.h2`
