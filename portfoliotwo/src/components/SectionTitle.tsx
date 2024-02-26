5f5bd6cc706905773c4399c93770c25763ca7163
Arefevpro <arefevmsk@gmail.com>
2024-02-26 11:43:45 +0300

    ./

--- portfoliotwo/src/components/SectionTitle.tsx
+++ portfoliotwo/src/components/SectionTitle.tsx
@@ -7,4 +7,14 @@ export const SectionTitle = styled.h2`
   font-size: 36px;
   font-weight: 600;
   letter-spacing: 5px;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 55px;
+    height: 1px;
+    background: ${props => props.theme.colors.accent};
+  }
+
+
 `;
