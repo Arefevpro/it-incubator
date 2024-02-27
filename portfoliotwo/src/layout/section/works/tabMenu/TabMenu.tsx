a14e83db446352382cf6c5a84c40b28edcc4fd22
Arefevpro <arefevmsk@gmail.com>
2024-02-27 15:03:26 +0300

    ./

--- portfoliotwo/src/layout/section/works/tabMenu/TabMenu.tsx
+++ portfoliotwo/src/layout/section/works/tabMenu/TabMenu.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from "styled-components"
-import { theme } from '../../../../styles/Theme';
-
+import { Link } from '../../../../components/Link';
 
 
 export const TabMenu = (props: { menuItems: Array<string> }) => {
@@ -34,30 +33,3 @@ const ListItem = styled.li`
   z-index: 1;
 `;
 
-const Link = styled.a`
-  font-weight: 400;
-  font-size: 14px;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  padding: 10px;
-  
-
-  &:hover {
-    &::before {
-      height: 10px;
-    }
-  }
-
-
-  &::before {
-    content: "";
-    display: inline-block;
-    background-color: ${theme.colors.accent};
-
-    position: absolute;
-    bottom: -6px;
-    left: 0;
-    right: 0;
-    z-index: -1;
-  }
-`;
