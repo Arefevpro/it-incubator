a64b9ec0bf8348bb994616d824261fd869b3edd0
Arefevpro <arefevmsk@gmail.com>
2024-02-29 20:29:23 +0300

    ./

--- portfoliotwo/src/layout/section/contacts/Contacts.tsx
+++ portfoliotwo/src/layout/section/contacts/Contacts.tsx
@@ -7,34 +7,31 @@ import { Container } from '../../../components/slider/Container';
 export const Contact = () => {
   return (
     <StyledContacts>
-      <SectionTitle>Contact</SectionTitle>
-      <StyledForm>
-        <Container>
-        <Field placeholder={"Name"}/>
-        <Field placeholder={"Subject"}/>
-        <Field placeholder={"Message"} as={"textarea"}/>
-        <Button type={"submit"}>Send message</Button>
-        </Container>
-      </StyledForm>
+      <Container>
+        <SectionTitle>Contact</SectionTitle>
+        <StyledForm>
+          <Field placeholder={"Name"} />
+          <Field placeholder={"Subject"} />
+          <Field placeholder={"Message"} as={"textarea"} />
+          <Button type={"submit"}>Send message</Button>
+        </StyledForm>
+      </Container>
     </StyledContacts>
   );
 };
 
 const StyledContacts = styled.section `
-  min-height: 40vh;
+
 `
 const StyledForm = styled.form `
-  max-width: 100%;
-  width: 40%;
-  min-height: 40vh;
+  max-width: 540px;
+  width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 16px;
   margin: 0 auto;
 `
 
 const Field = styled.input `
-  width: 80%;
-  margin: 10px;
-  padding: 10px;
+
 `
