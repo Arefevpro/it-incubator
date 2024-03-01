1d0aa0e65c2ecc6ecb1b6f310e97a7bdfa426e22
Arefevpro <arefevmsk@gmail.com>
2024-03-01 15:56:31 +0300

    ./

--- test/my-flex/src/components/text-field/TextField.tsx
+++ test/my-flex/src/components/text-field/TextField.tsx
@@ -5,7 +5,7 @@ import "./TextField.css"
 export const TextField = () => {
   return (
     <div className="text-field">
-      <input type="text" id="textField"/>
+      <input type="text" id="textField" placeholder='Some placeholder'/>
       <label htmlFor="textField">Some placeholder</label>
     </div>
   );
