b682837e82fbe23b93336f91780c648a0a312585
Arefevpro <arefevmsk@gmail.com>
2024-03-04 10:18:53 +0300

    ./

--- portfolio/src/components/Hashtag.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { theme } from '../styles/Theme';
-
-
-export const Hashtag = styled.a`
-  
-`
--- portfolio/src/components/hashtag/Hashtag.tsx
+++ portfolio/src/components/hashtag/Hashtag.tsx
@@ -1,16 +1,8 @@
 import styled from 'styled-components';
-
-type HashtagDecorPropsType = {
-  fontSize?: string;
-  color?: string;
-  margin?: string;
-  padding?: string;
-}
+import { theme } from '../../styles/Theme';
 
 
-export const HashtagDecor = styled.div<HashtagDecorPropsType>`
-  font-size: ${props => props.fontSize || "16px"};
-  color: ${props => props.color || "#C778DD"};
-  margin: ${props => props.margin || "0"};
-  padding: ${props => props.padding || "0"};
+export const Hashtag = styled.a`
+  color: ${theme.colors.accentGreen} ;
+  
 `
