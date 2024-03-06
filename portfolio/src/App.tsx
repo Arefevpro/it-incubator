0ccc8d7067c59984f00710e9b1be0bb6eb73a760
Arefevpro <arefevmsk@gmail.com>
2024-03-06 11:01:44 +0300

    ./

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import styled from 'styled-components';
 import { Header } from './layout/header/Header';
 import { MainSection } from './layout/home/sections/main/Main';
+import { QuoteSection } from './layout/quote/Quote';
 
 function App() {
     return (
       <div className="App">
           <Header />
           <MainSection />
+          <QuoteSection />
       </div>
     );
 }
--- portfolio/src/layout/quote/Quote.tsx
+++ portfolio/src/layout/quote/Quote.tsx
@@ -1,10 +1,36 @@
 import styled from 'styled-components';
+import { FlexWrapper } from '../../components/flex/FlexWrapper';
+import { theme } from '../../styles/Theme';
+import { Container } from '../../components/slider/Container';
 
 
 export const QuoteSection = ( ) => {
   return (
-    <div>
+    
+      <Container>
+        <StyledQuoteSection>
+      <FlexWrapper direction="column" align="center">
+        <Quote>With great power comes great electricity bill</Quote>
+        <Quote>- Dr. Who</Quote>
+      </FlexWrapper>
+      </StyledQuoteSection>
+      </Container>
       
-    </div>
+    
   );
 };
+
+
+const StyledQuoteSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
+`;
+
+
+const Quote = styled.p`
+padding: 32px;
+  font-size: 24px;
+  color: ${theme.colors.accentWhite};
+  border: 1px solid ${theme.colors.accentGreen};
+`
