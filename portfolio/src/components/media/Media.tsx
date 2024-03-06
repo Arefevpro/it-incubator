4311a64e1ee4f9b263787fc773bac3840148836f
Arefevpro <arefevmsk@gmail.com>
2024-03-06 17:46:21 +0300

    ./

--- portfolio/src/components/media/Media.tsx
+++ portfolio/src/components/media/Media.tsx
@@ -4,8 +4,38 @@ import styled from 'styled-components';
 export const Media = () => {
   return (
     <div>
-      
+      <MediaList>
+        <MediaItem>
+          <MedialLink></MedialLink>
+        </MediaItem>
+        <MediaItem>
+          <MedialLink></MedialLink>
+        </MediaItem>
+        <MediaItem>
+          <MedialLink></MedialLink>
+        </MediaItem>
+      </MediaList>
     </div>
   );
 };
 
+
+
+const MediaList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  `
+
+  const MediaItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  `
+
+  const MedialLink = styled.a`
+    
+
+  `
