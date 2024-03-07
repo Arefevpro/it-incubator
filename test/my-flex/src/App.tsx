62fe93421d04cf8bba2d237687405c4483cc9034
Arefevpro <arefevmsk@gmail.com>
2024-03-07 16:22:10 +0300

    ./

--- test/my-flex/src/App.tsx
+++ test/my-flex/src/App.tsx
@@ -1,56 +1,11 @@
 import React from 'react';
 import './App.css';
-import { TextField } from './components/text-field/TextField';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      {/* <div className="flex-container">
-        <div className="myDiv">I`m div</div>
-        <img
-          className="myImg"
-          src="https://www.pngmart.com/files/7/Discount-PNG-Background-Image.png"
-          alt=""
-        />
-        <input className="myInput" type="text" placeholder="i`m input" />
-        <p className="myText">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
-          quo assumenda? Corporis, perspiciatis, magnam quod eum recusandae
-          asperiores, debitis laudantium eligendi velit ex hic exercitationem
-          libero cupiditate quo iusto iure.
-        </p>
-        <span className="mySpan">i`m span</span>
-      </div> */}
-
-     {/* <div className="wrap">
-     <header>Header</header>
-      <main>Main</main>
-      <footer>Footer</footer>
-    </div> */}
-    
-{/* 
-    <ul>
-      <li>Первый</li>
-      <li>Второй</li>
-      <li>Последний</li>
-    </ul>
-    
-    <div>
-      <h2>Первый заголовок</h2>
-      <p>Первый абзац</p>
-      <h2>Второй заголовок</h2>
-      <p>Второй абзац</p>
-      <p>Третий абзац</p>
-      <h2>Последний заголовок</h2>
-      <p>Последний абзац</p>
-    </div> */}
-
-    
-    <TextField/>
-
-
-
-
+     
 
     
      </div>
---
+++ test/my-flex/src/Post.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+const Post = () => {
+  return (
+    <div>
+      <img src="/src/img/Adventure-PNG-Pic.png" alt="" />
+      <div>
+        <span>Like</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
