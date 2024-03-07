6700adea4b9c79bba9b5ccaf0df95513db160ba6
Arefevpro <arefevmsk@gmail.com>
2024-03-07 12:57:11 +0300

    ./

--- portfolio/src/components/menu/Menu.tsx
+++ portfolio/src/components/menu/Menu.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from 'styled-components';
-import { Hashtag } from "../hashtag/Hashtag";
 import { theme } from "../../styles/Theme";
+import { Hashtag } from '../hashtag/Hashtag';
 
 export const Menu = () => {
   return (
