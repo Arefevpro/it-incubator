4cd66bf88386c8fd5bb51acb2601dba75e5d1a63
Arefevpro <arefevmsk@gmail.com>
2024-03-10 10:01:23 +0300

    ./

--- portfolio/src/components/textComponents/SectionTitle.tsx
+++ portfolio/src/components/textComponents/SectionTitle.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { theme } from '../../styles/Theme';
-import { Hashtag } from '../hashtag/Hashtag';
 
 
 
-export const SectionTitle = () => {
-  return (
-    <Hashtag />
-  );
-};
-
-const Title = styled.h2`
+export const SectionTitle = styled.h2`
   font-size: 32px;
-  font-weight: 700;
-  color: ${theme.colors.accentWhite};
+  color: ${theme.colors.accentGreen};
 `
