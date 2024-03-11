86c3a71facc0938fe0943ed9a6cd8a2e4f5cb5b0
Arefevpro <arefevmsk@gmail.com>
2024-03-11 10:38:09 +0300

    ./

--- portfolio/src/layout/home/sections/skills/Skills.tsx
+++ portfolio/src/layout/home/sections/skills/Skills.tsx
@@ -8,19 +8,36 @@ import { Container } from '../../../../components/slider/Container';
 
 export const Skills = () => {
   return (
-    <SkillsWrapper>
-      <Container>
+    <Container>
+      <SkillsWrapper>
+        <DecorWrapper />
+        <CardWrapper>
 
-      </Container>
-    </SkillsWrapper>
+        </CardWrapper>
+      </SkillsWrapper>
+    </Container>
   );
 };
 
 
 const SkillsWrapper = styled.div`
   display: flex;
+  gap: 59px;
+  width: 100%;
+  height: 100vh;
+`;
+
+const DecorWrapper = styled.div`
+  min-width: 349px;
+  min-height: 282px;
+  width: 100%;
+  border: 1px solid ${theme.colors.colorGray};
+  `;
+
+
+const CardWrapper = styled.div`
+  display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
   border: 1px solid ${theme.colors.colorGray};
-`;
+`
