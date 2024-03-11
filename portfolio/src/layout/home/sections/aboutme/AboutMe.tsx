ca6426677ad264419fc5b1b8c1a3616ed73c16c6
Arefevpro <arefevmsk@gmail.com>
2024-03-11 16:09:52 +0300

    ./

---
+++ portfolio/src/layout/home/sections/aboutme/AboutMe.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import { theme } from '../../../../styles/Theme';
+import { Container } from '../../../../components/slider/Container';
+import { SectionTitle } from '../../../../components/textComponents/SectionTitle';
+import { Hashtag } from '../../../../components/hashtag/Hashtag';
+
+
+export const AboutMe = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
