fb298da12de6649ec84a624e18df6837c22b7c2c
Arefevpro <arefevmsk@gmail.com>
2024-03-11 11:25:19 +0300

    ./

--- portfolio/src/layout/home/sections/skills/Skills.tsx
+++ portfolio/src/layout/home/sections/skills/Skills.tsx
@@ -14,8 +14,13 @@ export const Skills = () => {
           <Hashtag name="#" />
           <SectionTitle title="skills" />
         </TitleWrapper>
-        <DecorWrapper />
-        <CardWrapper></CardWrapper>
+        <BoxWrapper>
+          <DecorWrapper>
+            <CubeDecorOne />
+            <CubeDecorTwo />
+          </DecorWrapper>
+          <CardWrapper></CardWrapper>
+        </BoxWrapper>
       </SkillsWrapper>
     </Container>
   );
@@ -26,23 +31,62 @@ const SkillsWrapper = styled.div`
 padding-top: 106px;
   display: flex;
   flex-direction: column;
-  gap: 59px;
+  gap: 12x;
   width: 100%;
   height: 100vh;
 `;
 
-
 const TitleWrapper = styled.div`
   display: flex;
   align-items: center;
+  width: 390px;
+   span {
+    font-size: 32px;
+   }
+
+   &::after {
+    content: "";
+    display: inline-block;
+    width: 100%;
+    height: 1px;
+    background-color: ${theme.colors.accentGreen};
+    margin-top: 8px;
+    margin-left: 16px;
+  }
+  `
+
+const BoxWrapper = styled.div`
+  display: flex;
+  gap: 58px;
   width: 100%;
+  height: 100%;
   `
 
 const DecorWrapper = styled.div`
-  width: 349px;
+  width: 350px;
   height: 282px;
   border: 1px solid ${theme.colors.colorGray};
+  position: relative;
   `;
+
+const CubeDecorOne = styled.div`
+width: 86px;
+height: 86px;
+border: 1px solid ${theme.colors.colorGray};
+position: absolute;
+top: 0;
+right: 36px;
+`;
+
+
+const CubeDecorTwo = styled.div`
+width: 52px;
+height: 52px;
+border: 1px solid ${theme.colors.colorGray};
+position: absolute;
+right: 0;
+bottom: 37px;
+`;
 
 
 const CardWrapper = styled.div`
@@ -51,3 +95,5 @@ const CardWrapper = styled.div`
   width: 584px;
   border: 1px solid ${theme.colors.colorGray};
 `
+
+
