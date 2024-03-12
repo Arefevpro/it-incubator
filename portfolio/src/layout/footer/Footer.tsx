f863a589984de1ecb4c5186c56afad4f4b42dd17
Arefevpro <arefevmsk@gmail.com>
2024-03-12 15:43:50 +0300

    ./

--- portfolio/src/layout/footer/Footer.tsx
+++ portfolio/src/layout/footer/Footer.tsx
@@ -4,14 +4,28 @@ import { theme } from '../../styles/Theme';
 import { Container } from '../../components/slider/Container';
 import { SectionTitle } from '../../components/textComponents/SectionTitle';
 import { Hashtag } from '../../components/hashtag/Hashtag';
+import { Logo } from '../../components/logo/Logo';
 
 
 
 
 export const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <StyledFooter>
+      <Container>
+        <Logo />
+      </Container>
+    </StyledFooter>
   );
 };
+
+
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  padding: 32px 0 8px;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 32px;
+  color: ${theme.colors.colorGray};
+  `
