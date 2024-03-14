9192ac6a722e596d742d0fa5b2f6688f25b42949
Arefevpro <arefevmsk@gmail.com>
2024-03-14 13:23:09 +0300

    ./

--- portfolio/src/pages/about/MyFunFacts.tsx
+++ portfolio/src/pages/about/MyFunFacts.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Container } from '../../components/slider/Container';
 import { SectionTitle } from '../../components/textComponents/SectionTitle';
 
@@ -6,7 +7,45 @@ export const MyFunFacts = () => {
   return (
     <Container>
       <SectionTitle title="my-fun-facts" />
+      <FactsSectionWrapper>
+        <FactsWrapper>
+          <FactsItem></FactsItem>
+          <FactsItem></FactsItem>
+          <FactsItem></FactsItem>
+          <FactsItem></FactsItem>
+          <FactsItem></FactsItem>
+          <FactsItem></FactsItem>
+          <FactsItem></FactsItem>
+        </FactsWrapper>
+        <DecoreWraper></DecoreWraper>
+      </FactsSectionWrapper>
     </Container>
   );
 };
 
+
+const FactsSectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const FactsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const FactsItem = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`;
+
+
+const DecoreWraper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+ `
