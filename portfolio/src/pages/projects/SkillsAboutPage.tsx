eed0d0f538b1f6ce8c52418d56c6246543ba9b8f
Arefevpro <arefevmsk@gmail.com>
2024-03-14 12:20:32 +0300

    ./

--- portfolio/src/pages/projects/SkillsAboutPage.tsx
+++ portfolio/src/pages/projects/SkillsAboutPage.tsx
@@ -4,13 +4,47 @@ import { theme } from '../../styles/Theme';
 import { Container } from '../../components/slider/Container';
 import { SectionTitle } from '../../components/textComponents/SectionTitle';
 import { Hashtag } from '../../components/hashtag/Hashtag';
+import { SkillCardHome } from '../../pages/home/skills/SkillCardHome';
 
 
 export const SkillsAboutPage = () => {
   return (
     <Container>
       <SectionTitle title="skills" />
+      <CardWrapper>
+            <SkillCardHome
+              name="Languages"
+              description="TypeScript Lua Python JavaScript"
+            />
+            <SkillCardHome
+              name="Databases" 
+              description="SQLite PostgreSQL Mongo" />
+            <SkillCardHome
+              width={196}
+              name="Tools"
+              description="VSCode Neovim Linux Figma XFCE Arch Git Font Awesome"
+            />
+            <SkillCardHome
+              name="Other"
+              description="HTML CSS EJS SCSS REST Jinja"
+            />
+            <SkillCardHome
+              width={196}
+              name="Frameworks"
+              description="React Vue Disnake Discord.js Flask Express.js"
+            />
+          </CardWrapper>
     </Container>
   );
 };
 
+
+const CardWrapper = styled.div`
+  padding-top: 48px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  align-items: flex-start;
+  align-content: flex-start;
+  gap: 16px;
+`;
