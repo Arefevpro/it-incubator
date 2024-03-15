45d708aa16d1a2bb1f69ee0707567ab4a0f79628
Arefevpro <arefevmsk@gmail.com>
2024-03-15 10:38:43 +0300

    ./

--- portfolio/src/components/allMedia/AllMedia.tsx
+++ portfolio/src/components/allMedia/AllMedia.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 import { theme } from '../../styles/Theme';
 import { Container } from '../slider/Container';
+import { Twitter } from'../media/Twitter';
 
 export const AllMedia = () => {
   return (
     <Container>
       <AllMediaWrapper>
-        
+        <Twitter />
+        <Twitter />
       </AllMediaWrapper>
     </Container>
   );
 };
 
+
+const AllMediaWrapper = styled.div`
+  max-width: 1064px;
+  width: 100%;
+  min-height: 100%;
+  padding: 0 20px;
+  margin: 0 auto;
+  /* outline: 1px solid ${theme.colors.accentGreen}; */
+`;
---
+++ portfolio/src/components/media/Twitter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import {  theme } from '../../styles/Theme';
+import { Container } from '../slider/Container';
+import { Icon } from '../icon/Icon';
+
+export const Twitter = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
