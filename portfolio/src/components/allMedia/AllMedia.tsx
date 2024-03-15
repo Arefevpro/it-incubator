fa1d5df52011997f53949dada2b5338aa4680fb6
Arefevpro <arefevmsk@gmail.com>
2024-03-15 10:07:35 +0300

    ./

---
+++ portfolio/src/components/allMedia/AllMedia.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllMedia = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default AllMedia;
--- portfolio/src/components/footer/Footer.tsx
+++ portfolio/src/components/footer/Footer.tsx
@@ -2,69 +2,63 @@ import React from 'react';
 import styled from 'styled-components';
 import { theme } from '../../styles/Theme';
 import { Container } from '../slider/Container';
-import { SectionTitle } from '../textComponents/SectionTitle';
-import { Hashtag } from '../hashtag/Hashtag';
 import { Logo } from '../logo/Logo';
 import { Icon } from '../icon/Icon';
-
-
 
 
 export const Footer = () => {
   return (
     <DecorWrapper>
+      <Container>
+        <StyledFooterWrapper>
+          <StyledFooter>
+            <SignatureWrapper>
+              <LogoWrapper>
+                <Logo />
+                <Email>elias@elias-dev.ml</Email>
+              </LogoWrapper>
+              <Signature>Web designer and front-end developer</Signature>
+            </SignatureWrapper>
 
-   
-    <Container>
-      <StyledFooterWrapper>
-        <StyledFooter>
-          <SignatureWrapper>
-            <LogoWrapper>
-              <Logo />
-              <Email>elias@elias-dev.ml</Email>
-            </LogoWrapper>
-            <Signature>Web designer and front-end developer</Signature>
-          </SignatureWrapper>
-
-          <StyledMedia>
-            <MediaTitle>Media</MediaTitle>
-            <MediaList>
-              <MediaItem>
-                <MedialLink>
-                  <Icon
-                    iconId={"github"}
-                    width={"32px"}
-                    height={"32px"}
-                    viewbox={"0 0 32 32"}
-                  />
-                </MedialLink>
-              </MediaItem>
-              <MediaItem>
-                <MedialLink>
-                  <Icon
-                    iconId={"dribble"}
-                    width={"32px"}
-                    height={"32px"}
-                    viewbox={"0 0 32 32"}
-                  />
-                </MedialLink>
-              </MediaItem>
-              <MediaItem>
-                <MedialLink>
-                  <Icon
-                    iconId={"figma"}
-                    width={"32px"}
-                    height={"32px"}
-                    viewbox={"0 0 32 32"}
-                  />
-                </MedialLink>
-              </MediaItem>
-            </MediaList>
-          </StyledMedia>
-        </StyledFooter>
-        <Copyright>© Copyright 2022. Made by Elias</Copyright>
-      </StyledFooterWrapper>
-    </Container>
+            <StyledMedia>
+              <MediaTitle>Media</MediaTitle>
+              <MediaList>
+                <MediaItem>
+                  <MedialLink>
+                    <Icon
+                      iconId={"github"}
+                      width={"32px"}
+                      height={"32px"}
+                      viewbox={"0 0 32 32"}
+                    />
+                  </MedialLink>
+                </MediaItem>
+                <MediaItem>
+                  <MedialLink>
+                    <Icon
+                      iconId={"dribble"}
+                      width={"32px"}
+                      height={"32px"}
+                      viewbox={"0 0 32 32"}
+                    />
+                  </MedialLink>
+                </MediaItem>
+                <MediaItem>
+                  <MedialLink>
+                    <Icon
+                      iconId={"figma"}
+                      width={"32px"}
+                      height={"32px"}
+                      viewbox={"0 0 32 32"}
+                    />
+                  </MedialLink>
+                </MediaItem>
+              </MediaList>
+            </StyledMedia>
+          </StyledFooter>
+          <Copyright>© Copyright 2022. Made by Elias</Copyright>
+        </StyledFooterWrapper>
+      </Container>
     </DecorWrapper>
   );
 };
