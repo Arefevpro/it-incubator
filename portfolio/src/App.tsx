8571dfcd527c7cdb5f7dd532f24ff61adb4c65fe
Arefevpro <arefevmsk@gmail.com>
2024-03-20 18:12:34 +0300

    ./

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -20,7 +20,7 @@ import { CompletApps } from './components/CompletApps';
 function App() {
     return (
       <div className="App">
-        <HomePage>
+        {/* <HomePage>
           <Header />
           <Media />
           <MainSection />
@@ -48,11 +48,94 @@ function App() {
           <MyFunFacts />
           <Footer />
         </AboutPage>
-        <ContactsPage />
+        <ContactsPage /> */}
+
+
+<Appo>
+<Header>Header</Header>
+<Aside>Aside</Aside>
+<Main>
+<Wrapper>
+  <Box>Box</Box>
+</Wrapper>
+<Content>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem fugiat nisi quaerat soluta tenetur blanditiis, nemo ducimus commodi consequuntur animi aliquam aliquid mollitia odit perferendis cupiditate pariatur maxime molestias tempora.
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem fugiat nisi quaerat soluta tenetur blanditiis, nemo ducimus commodi consequuntur animi aliquam aliquid mollitia odit perferendis cupiditate pariatur maxime molestias tempora.
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem fugiat nisi quaerat soluta tenetur blanditiis, nemo ducimus commodi consequuntur animi aliquam aliquid mollitia odit perferendis cupiditate pariatur maxime molestias tempora.
+</Content>
+</Main>
+</Appo>
+
       </div>
     );
 }
 
+
+const Appo = styled.div`
+  display: grid;
+  grid-template-columns: 20% 80%;
+  grid-template-rows: 60px 1fr;
+  grid-template-areas:
+    "header header"
+    "aside main";
+  height: 100vh;
+
+
+`
+
+const Header = styled.header`
+  
+  
+`
+
+const Aside = styled.aside`
+  grid-area: aside;
+  background-color: #000;
+  color: #fff;
+`
+
+
+const Main = styled.main`
+  grid-area: main;
+  background-color: #000;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+` 
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: #fff;
+  color: #000;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 200px;
+  border-radius: 20px;
+  margin-bottom: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    transform: scale(1.1);
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+  }
+`
 
 
 
