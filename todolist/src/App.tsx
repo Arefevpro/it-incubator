f10cdde0608737e904661a4e80f8667e657ebd3a
Arefevpro <arefevmsk@gmail.com>
2024-03-23 13:18:51 +0300

    ./

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
-import { Todolist } from './Todolist';
+import "./App.css";
+import { Todolist } from "./Todolist";
 
 function App() {
-    return (
-        <div className="App">
-           
-        </div>
-    );
+  return (
+    <div className="App">
+      <Todolist title="What to learn"/>
+      <Todolist title="Songs"/>
+      <Todolist title="Books"/>
+    </div>
+  );
 }
 
 export default App;
