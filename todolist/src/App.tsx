d9b07e1a9fd1a3c7eb1c6a8d40f99e9b00863d77
Arefevpro <arefevmsk@gmail.com>
2024-03-25 16:36:01 +0300

    ./

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -9,12 +9,14 @@ function App() {
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'ReactJS', isDone: false },
+    { id: 4, title: 'Redux', isDone: false },
   ]
  
   const tasks2 = [
     { id: 1, title: 'Hello world', isDone: true },
     { id: 2, title: 'I am Happy', isDone: false },
     { id: 3, title: 'Yo', isDone: false },
+    { id: 4, title: 'Redux', isDone: false },
   ]
  
   return (
