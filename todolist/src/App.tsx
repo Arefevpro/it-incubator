7b8d3d2d17d712191a740733621defc81987d5bc
Arefevpro <arefevmsk@gmail.com>
2024-03-25 20:33:55 +0300

    ./

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -5,22 +5,10 @@ import { Todolist } from "./Todolist";
 
 
 function App() {
-  const tasks1 = [
-    { id: 1, title: 'HTML&CSS', isDone: true },
-    { id: 2, title: 'JS', isDone: true },
-    { id: 3, title: 'ReactJS', isDone: false },
-    { id: 4, title: 'Redux', isDone: false },
-    { id: 5, title: 'Typescript', isDone: false },
-    { id: 6, title: 'RTK query', isDone: false },
-  ]
- 
-  const tasks2 = [
-  ]
- 
   return (
     <div className="App">
-      <Todolist title="What to learn" tasks={tasks1} />
-      <Todolist title="Songs" tasks={tasks2} date="22.02.2022" />
+      <Todolist  />
+      <Todolist />
     </div>
   )
 }
--- todolist/src/Todolist.tsx
+++ todolist/src/Todolist.tsx
@@ -1,55 +1,111 @@
 
-type PropsType = {
-  title: string
-  tasks: TaskType[]
-  date?: string
-}
 
 
-type TaskType = {
-  id: number
-  title: string
-  isDone: boolean
-}
-
-export const Todolist = ({ title, tasks, date }: PropsType) => {
-
+ 
+export const Todolist = () => {
   return (
     <div>
-    <h3>{title}</h3>
-    <div>
-        <input/>
-        <Button title={'+'} />
+      <h3>What to learn</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        <li>
+          <input type="checkbox" checked={true} /> <span>HTML&CSS</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={true} /> <span>JS</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={false} /> <span>React</span>
+        </li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
     </div>
-    {tasks.length === 0 ? (
-        <p>Тасок нет</p>
-      ) : (
-        <ul>
-          {tasks.map(task => {
-            return (
-              <li key={task.id}>
-                <input type="checkbox" checked={task.isDone} />
-                <span>{task.title}</span>
-              </li>
-            )
-          })}
-        </ul>
-      )}
-    <div>
-    <Button title={'All'} />
-        <Button title={'Active'} />
-        <Button title={'Completed'} />
-    </div>
-    <div>{date}</div>
-</div>
-  );
-};
-
-
-type ButtonPropsType = {
-  title: string
+  )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// type PropsType = {
+//   title: string
+//   tasks: TaskType[]
+//   date?: string
+// }
+
+
+// type TaskType = {
+//   id: number
+//   title: string
+//   isDone: boolean
+// }
+
+// export const Todolist = ({ title, tasks, date }: PropsType) => {
+
+//   return (
+//     <div>
+//     <h3>{title}</h3>
+//     <div>
+//         <input/>
+//         <Button title={'+'} />
+//     </div>
+//     {tasks.length === 0 ? (
+//         <p>Тасок нет</p>
+//       ) : (
+//         <ul>
+//           {tasks.map(task => {
+//             return (
+//               <li key={task.id}>
+//                 <input type="checkbox" checked={task.isDone} />
+//                 <span>{task.title}</span>
+//               </li>
+//             )
+//           })}
+//         </ul>
+//       )}
+//     <div>
+//     <Button title={'All'} />
+//         <Button title={'Active'} />
+//         <Button title={'Completed'} />
+//     </div>
+//     <div>{date}</div>
+// </div>
+//   );
+// };
+
+
+// type ButtonPropsType = {
+//   title: string
+// }
  
-export const Button = ({ title }: ButtonPropsType) => {
-  return <button>{title}</button>
-}
+// export const Button = ({ title }: ButtonPropsType) => {
+//   return <button>{title}</button>
+// }
