109249b2faf3c9ab94117a2aa2eb3bd45949688c
Arefevpro <arefevmsk@gmail.com>
2024-04-02 10:11:11 +0300

    ./

--- portfolio/src/styles/Theme.ts
+++
@@ -1,9 +0,0 @@
-export const theme = {
-  colors: {
-    accentGreen: "#00f32d", // Зелый
-    accentWhite: "#FFFFFF", // Белый
-    colorGray: "#ABB2BF",   // Серый 
-
-    bgColorDark: "#282C33", // цвет фона поумолчанию
-  },
-};
