b1d0edef099193e1c36eb0fff84d45468dd37846
Arefevpro <arefevmsk@gmail.com>
2024-04-05 09:20:52 +0300

    ./

--- js-native/src/App.tsx
+++ js-native/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
 
 export default App;
+
+function calculateNumber(initialValue: number) { // initialValue - параметр функции типа number
+  let x = initialValue;
+  x = x + 100;
+  x = x / 5;
+
+  return x;
+}
+
+const result = calculateNumber(5334535);
+const result1 = calculateNumber(5335);
+console.log(result);
+console.log(result1);
+
+function calculateCercleArea(cercleRadius: number) { // initialValue - параметр функции типа number
+return 3.14 * cercleRadius ** 2;
+}
+
+const cercleArea1 = calculateCercleArea(6);
+console.log(cercleArea1);
+
+const cercleArea2 = calculateCercleArea(16.2);
+console.log(cercleArea2);
