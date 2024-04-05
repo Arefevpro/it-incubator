4170f6b920a9cfec65c4aaca73bd2c29e9f0e7b2
Arefevpro <arefevmsk@gmail.com>
2024-04-05 09:53:16 +0300

    ./

--- js-native/src/App.tsx
+++ js-native/src/App.tsx
@@ -33,3 +33,16 @@ const cercleArea1 = calculateCercleArea(6); // (6) - это аргумент (з
 const cercleArea2 = calculateCercleArea(16.2);
 // console.log(cercleArea2);
 
+
+
+
+function createHTMLElement(tag: string, text: string, id: string) {
+  const element = document.createElement(tag);
+  element.id =id;
+  element.innerHTML = text;
+  
+  return element;
+}
+
+const h1element = createHTMLElement('h1', 'header1', 'text');
+console.log(h1element)
