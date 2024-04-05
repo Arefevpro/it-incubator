608f0f05c45680deeb031f6d23a773e643dd5bf0
Arefevpro <arefevmsk@gmail.com>
2024-04-05 20:23:45 +0300

    ./

--- js-native/src/App.tsx
+++ js-native/src/App.tsx
@@ -85,3 +85,16 @@ sayBye();
 
 
 
+const brocenLinks = [vk, youtybe, instagram, facebook];
+// https://vk.com
+
+const fixLinksArray = (linksArray) => {  
+  const correctLinks = [];                 // коллбэк функция - это функция переданная в другую функцию в качестве аргумента
+  linksArray.forEach(function(brokenLink) {
+    const fixedLink = 'https://' + brokenLink + '.com';
+    correctLinks.push(fixedLink);
+  })
+  return correctLinks;  
+}
+
+console.log(fixLinksArray(brocenLinks));
