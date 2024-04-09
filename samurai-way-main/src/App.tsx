549b6e3df3ffbb9449dd4789a5078a169cc3f02e
Arefevpro <arefevmsk@gmail.com>
2024-04-09 14:42:13 +0300

    ./

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -1,32 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-     <Header />
-     <h1>Hello, samurai! Let's go!</h1>
-     <ul>
-       <li>HTML</li>
-       <li>CSS</li>
-       <li>JS</li>
-       <li>React</li>
-       <li>Redux</li>
-       <li>Typescript</li>
-     </ul>
+      <Header />
+      <h1>Hello, samurai! Let's go!</h1>
+      <Tachnologies />
     </div>
   );
-}
-// 89614622994 магомед
-export default App;
+};
 
+export default App;
 
 const Header = () => {
   return (
     <div>
-      <a href="Home">Home</a>
-      <a href="Projects">Projects</a>
-      <a href="About me">About me</a>
+      <a href="#c">Home</a>
+      <a href="#c">Projects</a>
+      <a href="#c">About me</a>
     </div>
+  );
+};
+
+const Tachnologies = () => {
+  return (
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JS</li>
+      <li>React</li>
+      <li>Redux</li>
+      <li>Typescript</li>
+    </ul>
   )
-}
+};
