938db015b069060362da03610dca11b0396d6af6
Arefevpro <arefevmsk@gmail.com>
2024-04-09 14:31:30 +0300

    ./

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -1,19 +1,32 @@
+import React from 'react';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JS</li>
-      <li>React</li>
-      <li>Redux</li>
-    </ul>
+     <Header />
+     <h1>Hello, samurai! Let's go!</h1>
+     <ul>
+       <li>HTML</li>
+       <li>CSS</li>
+       <li>JS</li>
+       <li>React</li>
+       <li>Redux</li>
+       <li>Typescript</li>
+     </ul>
     </div>
   );
 }
-
+// 89614622994 магомед
 export default App;
 
+
+const Header = () => {
+  return (
+    <div>
+      <a href="Home">Home</a>
+      <a href="Projects">Projects</a>
+      <a href="About me">About me</a>
+    </div>
+  )
+}
