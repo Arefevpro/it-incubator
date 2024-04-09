115e7739d352591b4790f6f3edcaf77f686fb6fe
Arefevpro <arefevmsk@gmail.com>
2024-04-09 17:26:52 +0300

    ./

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import { Header } from "./Header";
-import {Tachnologies} from "./Tachnologies";
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <h1>Hello, samurai! Let's go!</h1>
-      <Tachnologies />
+      
     </div>
   );
 };
