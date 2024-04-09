1b2b7c1e24229ecfb76b077fcc55c17509c0e83b
Arefevpro <arefevmsk@gmail.com>
2024-04-09 17:43:22 +0300

    ./

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -2,11 +2,28 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      
+    <div className="app-wrapper">
+      <header>
+        <img
+          src="https://e7.pngegg.com/pngimages/41/701/png-clipart-logo-graphic-design-graphy-grapher-logo-graphy-angle-text.png"
+          alt="logo"
+          width="100px"
+          height="100px"
+        />
+      </header>
+      <nav>
+        <ul>
+          <li>Profile</li>
+          <li>Messages</li>
+          <li>Photo</li>
+        </ul>
+      </nav>
+      <main>
+        Main content
+      </main>
+      <footer></footer>
     </div>
   );
 };
 
 export default App;
-
