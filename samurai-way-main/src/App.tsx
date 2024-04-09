341febedba056757ad1708ab470bc44e559ba46a
Arefevpro <arefevmsk@gmail.com>
2024-04-09 17:05:55 +0300

    ./

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import "./App.css";
-import {Header} from "./Header";
+import { Header } from "./Header";
 import {Tachnologies} from "./Tachnologies";
 
 const App = () => {
