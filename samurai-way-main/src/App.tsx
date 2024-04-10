48c379bc26688dfaa404d5de51a5080e44b2418a
Arefevpro <arefevmsk@gmail.com>
2024-04-10 11:38:18 +0300

    ./

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -16,10 +16,23 @@ const App = () => {
           <li><a href="#c">Profile</a></li>
           <li><a href="#c">Messages</a></li>
           <li><a href="#c">Photo</a></li>
+          <li><a href="#c">Music</a></li>
+          <li><a href="#c">Settings</a></li>  
         </ul>
       </nav>
       <main className="content">
-        Main content
+        <div><img src="https://rd-prod.twic.pics/2023-09-06/PHdX6azeavPRGpPDFxXDB3e5TSoPGFOUZiEuqJLB.webp?twic=v1/resize=828/quality=73/focus=auto" alt="" /></div>
+        <div>
+          <div>img</div>
+          <div>description</div>
+        </div>
+        <div>
+          <h2>my posts</h2>
+          <div>new post</div>
+          <div>post1</div>
+          <div>post2</div>
+          <button>sedd</button>
+        </div>
       </main>
       <footer className="footer">Footer</footer>
     </div>
